6a2bcdac39b27ae07d86b76f06dcd76c775673ff
Parinitha03 <58803905+Parinitha03@users.noreply.github.com>
2024-04-03 15:06:10 -0400

    Add files via upload

---
+++ ArrayCount.cs
@@ -0,0 +1,21 @@
+﻿int[] myArr = {1,1,2,2,2,3,3,3,3,5,5,6,7,8};
+
+Dictionary<int, int> myHashMap = new Dictionary<int, int>();
+for (var i = 0; i < myArr.Length; i++ ){
+    int myKey = myArr[i]; 
+    if (myHashMap.ContainsKey(myKey)){
+        myHashMap[myKey] += 1;
+    }else{
+        myHashMap[myKey] = 1;
+    }
+}
+
+foreach (KeyValuePair<int, int> kvp in myHashMap){
+    Console.Write(kvp.Key+"="+kvp.Value+" ");
+} 
+
+
+
+
+
+
